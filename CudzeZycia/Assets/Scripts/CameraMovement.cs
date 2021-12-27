1d1f1256a4e4aa700f2c95456fa857d66d64cecd
PurpleCatChowder <maksymilianzajac18@gmail.com>
2021-12-27 19:29:11 +0100

    CameraMovement

---
+++ CudzeZycia/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] public Transform Target;
+    [SerializeField] private Vector3 Offset;
+    [SerializeField] public float Distance;
+    [SerializeField] public float Height;
+    [SerializeField] public float speedCamera;
+
+   
+
+    private void LateUpdate()
+    {
+        Offset = new Vector3(-Distance, Height, -Distance);
+        Vector3 DistancePosition = Target.position + Offset;
+        Vector3 SmoothPosition = Vector3.Lerp(transform.position, DistancePosition, speedCamera);
+
+        transform.LookAt(Target);
+        transform.position = SmoothPosition;
+    }
+}
