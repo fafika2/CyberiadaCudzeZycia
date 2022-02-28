25ff99ae48ba3c183d25300234edfeda80074d63
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2022-02-28 22:56:36 +0100

    Dialogi v2 GIGACHAD

---
+++ CudzeZycia/Assets/Scripts/DialogSystem/Nodes/DialogAnswer.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using XNode;
+using UnityEngine;
+
+
+namespace Scripts.DialogSystem
+{
+
+    [Serializable]
+    public class DialogAnswer
+    {
+        public string Message;        
+
+        public enum AnswerType
+        {
+            MainQuest = 1,
+            Secondary = 2,
+            Gray = 3,
+        }
+        public AnswerType Type = AnswerType.Secondary;
+
+        
+        public Color GetTextColor()
+        {
+            Color myColor;
+            if (Type == AnswerType.MainQuest)
+            {
+                ColorUtility.TryParseHtmlString("#fca548", out myColor);
+            }
+            else if (Type == AnswerType.Secondary)
+            {
+                ColorUtility.TryParseHtmlString("#61ccff", out myColor);
+            }
+            else
+            {
+                ColorUtility.TryParseHtmlString("#989898", out myColor);
+            }
+            return myColor;
+        }
+
+    }
+
+}
