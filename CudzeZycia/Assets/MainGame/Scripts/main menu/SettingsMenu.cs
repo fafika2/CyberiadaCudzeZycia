9e6bc8fb80cc17f4323af10d0d4809c5035cb2f6
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2022-06-08 11:33:44 +0200

    Ustawienia gry, SettingsMenu.prefab, Poprawki w credits

--- CudzeZycia/Assets/MainGame/Scripts/main menu/SettingsMenu.cs
+++ CudzeZycia/Assets/MainGame/Scripts/main menu/SettingsMenu.cs
@@ -1,11 +1,66 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    public Dropdown qualityDropdown;
+    public Toggle fullscreenToggle;
+
+    private Resolution[] resolutions;
+
+
+    void Start()
+    {
+        // set options in resolutionDropdown and set default resolution
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        var options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            // TODO: remove weird resolutions from there so players cant choice freezer screen resolution lol - @GorlikItsMe
+            var res = resolutions[i];
+            options.Add(res.width + " x " + res.height);
+            if (Screen.currentResolution.Equals(res))
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+
+        // set default quality in dropdown
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+
+        // set default fullscrean
+        fullscreenToggle.isOn = Screen.fullScreen;
+    }
+
     public void SetVolume(float volume)
     {
-        Debug.Log("xd");
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityId)
+    {
+        QualitySettings.SetQualityLevel(qualityId);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resId)
+    {
+        var res = resolutions[resId];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
     }
 }
